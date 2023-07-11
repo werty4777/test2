fb8706c3478581f16ef153520152c5e94d9fd903
werty4777 <agalindoo@autonoma.edu.pe>
2023-07-10 19:17:46 -0500

    aasdds

--- src/app/services/apiservice.ts
+++ src/app/services/apiservice.ts
@@ -10,11 +10,11 @@ import * as url from "url";
   providedIn: 'root'
 })
 export class ApiService {
-  private urlproductos='https://00c9-38-25-18-197.ngrok-free.app/productos'
-  private urlaccesorios='https://00c9-38-25-18-197.ngrok-free.app/Accesorios'
-  private urlvendidos='https://00c9-38-25-18-197.ngrok-free.app/masvendidos'
-  private urlLogin='https://00c9-38-25-18-197.ngrok-free.app/login'
-  private urlRegister='https://00c9-38-25-18-197.ngrok-free.app/registrar'
+  private urlproductos='https://6113-38-25-18-121.ngrok-free.app/productos'
+  private urlaccesorios='https://6113-38-25-18-121.ngrok-free.app/Accesorios'
+  private urlvendidos='https://6113-38-25-18-121.ngrok-free.app/masvendidos'
+  private urlLogin='https://6113-38-25-18-121.ngrok-free.app/login'
+  private urlRegister='https://6113-38-25-18-121.ngrok-free.app/registrar'
 
   logeadoUser=new BehaviorSubject(false);
   $obsLogeado=this.logeadoUser.asObservable();
